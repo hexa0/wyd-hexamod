8fe2393e4853c711e51be6d97a0ca7e9a664e8d7
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-04-27 08:33:38 -0400

    this is ugly but required for building the relay on linux

---
+++ VoiceChatSharedNet9/Net/VoiceChatClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using HexaVoiceChatShared.MessageProtocol;
+
+namespace HexaVoiceChatShared.Net
+{
+    public class VoiceChatClient : VoiceChatNetBase
+    {
+        public VoiceChatClient(IPEndPoint remote)
+        {
+            Console.WriteLine($"VoiceChatClient: Connected to port {remote.Port} at {remote.Address}");
+            endPoint = remote;
+            socket = new UdpClient();
+            onMessageAction = delegate (DecodedVoiceChatMessage message, IPEndPoint endPoint)
+            {
+                if (onMessageActions.ContainsKey(message.type))
+                {
+                    onMessageActions[message.type].Invoke(message, endPoint);
+                }
+                else
+                {
+                    Console.WriteLine($"VoiceChatMessageType of \"{message.type}\" wasn't handled by the VoiceChatClient");
+                }
+            };
+
+            Connect(true);
+        }
+    }
+}
---
+++ VoiceChatSharedNet9/Net/VoiceChatServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using HexaVoiceChatShared.MessageProtocol;
+
+namespace HexaVoiceChatShared.Net
+{
+    public class VoiceChatServer : VoiceChatNetBase
+    {
+        public VoiceChatServer(IPEndPoint remote)
+        {
+            Console.WriteLine($"VoiceChatServer: Listening on port {remote.Port} from {remote.Address}");
+            endPoint = remote;
+            socket = new UdpClient(endPoint);
+            onMessageAction = delegate (DecodedVoiceChatMessage message, IPEndPoint endPoint)
+            {
+                if (onMessageActions.ContainsKey(message.type))
+                {
+                    onMessageActions[message.type].Invoke(message, endPoint);
+                }
+                else
+                {
+                    Console.WriteLine($"VoiceChatMessageType of \"{message.type}\" wasn't handled by the VoiceChatServer");
+                }
+            };
+
+            Connect(false);
+        }
+    }
+}
