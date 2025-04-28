51c7b7f6bc62b62f18ca5219090b27870354adbc
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-04-27 22:10:21 -0400

    fix gates

---
+++ HexaMod/Patches/Fixes/BabyGateBarsFix.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace HexaMod.Patches
+{
+    [HarmonyPatch(typeof(GateBars))]
+    internal class BabyGateBarsFix
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void Update(ref GateBars __instance)
+        {
+            __instance.transform.localPosition = Vector3.zero;
+            __instance.transform.localRotation = Quaternion.identity;
+        }
+    }
+}
