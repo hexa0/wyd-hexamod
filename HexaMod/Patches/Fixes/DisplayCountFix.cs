9d5d50cb4053ef4429c3d0d006524d872e980eda
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-04-28 17:18:08 -0400

    fix the display number being off by 1

---
+++ HexaMod/Patches/Fixes/DisplayCountFix.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+
+namespace HexaMod.Patches
+{
+    [HarmonyPatch(typeof(OptionsController))]
+    internal class DisplayCountFix
+    {
+        // originally the game would incorrectly display this zero indexed
+        // this confused the hell outta me so this fixes that, also user facing settings should be zero indexed????
+
+        [HarmonyPatch("ChangeDisplayUp")]
+        [HarmonyPostfix]
+        static void ChangeDisplayUp(ref OptionsController __instance)
+        {
+            __instance.dispText.text = (__instance.curDisplay + 1).ToString();
+        }
+
+        [HarmonyPatch("ChangeDisplayDown")]
+        [HarmonyPostfix]
+        static void ChangeDisplayDown(ref OptionsController __instance)
+        {
+            __instance.dispText.text = (__instance.curDisplay + 1).ToString();
+        }
+
+        [HarmonyPatch("Reset")]
+        [HarmonyPostfix]
+        static void Reset(ref OptionsController __instance)
+        {
+            __instance.dispText.text = (__instance.curDisplay + 1).ToString();
+        }
+    }
+}
