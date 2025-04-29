69bd7a4297a5a3e7a8d9aca78f81a4c98fa91170
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-04-28 21:38:18 -0400

    fix achievements causing some code to exit early

---
+++ HexaMod/Patches/Fixes/OfflineAchievementFix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using HarmonyLib;
+using Steamworks;
+using UnityEngine;
+
+namespace HexaMod.Patches
+{
+    [HarmonyPatch(typeof(AchievementManager))]
+    internal class OfflineAchievementFix
+    {
+        [HarmonyPatch("UnlockAchievement")]
+        [HarmonyPrefix]
+        static bool UnlockAchievement(ref AchievementManager __instance, int input)
+        {
+            string achievement = "ACHIEVEMENT_" + (input + 1).ToString("000");
+            PlayerPrefs.SetInt(achievement, 1);
+            MonoBehaviour.print(achievement);
+            MonoBehaviour.print("Achievement Unlocked: " + __instance.achievements[input].name);
+            UnlockAchievementSteam(achievement);
+
+            return false;
+        }
+
+        static void UnlockAchievementSteam(string name)
+        {
+            try
+            {
+                SteamUserStats.SetAchievement(name);
+                SteamUserStats.StoreStats();
+            }
+            catch (Exception e)
+            {
+                Mod.Warn("failed to unlock an achievement on the steam side, this error has been skipped to avoid game breaking bugs when playing offline/without launching through steam");
+            }
+        }
+    }
+}
