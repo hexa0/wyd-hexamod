1c1e8a4cab789bc832cf1258ca2a8bab771a621d
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-05-10 20:58:10 -0400

    fix mirrors

---
+++ HexaMod/Patches/Fixes/UpsideDownMirrorFix.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace HexaMod.Patches
+{
+	[HarmonyPatch(typeof(MirrorCam))]
+	internal class UpsideDownMirrorFix
+	{
+		[HarmonyPatch("Start")]
+		[HarmonyPostfix]
+		public static void Start(ref MirrorCam __instance)
+		{
+			__instance.transform.rotation *= Quaternion.Euler(0, 0, 180);
+		}
+	}
+}
