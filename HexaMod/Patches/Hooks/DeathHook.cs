5d76939374dd7f81f00a627fc241c9a1c009f58d
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-05-28 01:07:50 -0400

    you can throw them now (peak)

---
+++ HexaMod/Patches/Hooks/DeathHook.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using HexaMapAssemblies;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace HexaMod.Patches.Hooks
+{
+	internal class DeathRPC : MonoBehaviour
+	{
+		bool isDead = false;
+		[PunRPC]
+		public void DeadRPC()
+		{
+			if (isDead) return;
+			isDead = true;
+
+			gameObject.name = "Dead Baby";
+			Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
+			rigidBody.interpolation = RigidbodyInterpolation.Interpolate;
+			rigidBody.isKinematic = false;
+			rigidBody.useGravity = true;
+			DestroyImmediate(gameObject.GetComponent<NetworkMovement>());
+			gameObject.AddComponent<NetworkMovementRB>();
+			BoxCollider collider = gameObject.GetComponent<BoxCollider>();
+			collider.isTrigger = false;
+			collider.center = new Vector3(0f, -0.13f, -0.3f);
+			collider.size = new Vector3(0.5f, 0.2f, 1.2f);
+			PickUpFactory pickup = gameObject.AddComponent<PickUpFactory>();
+			pickup.babyCannotGrab = true;
+			gameObject.tag = "Untagged";
+		}
+	}
+
+	[HarmonyPatch]
+	internal class DeathHook
+	{
+		[HarmonyPatch(typeof(BabyStats), "Start")]
+		[HarmonyPostfix]
+		static void Start(ref BabyStats __instance)
+		{
+			__instance.gameObject.AddComponent<DeathRPC>();
+		}
+
+		[HarmonyPatch(typeof(BabyStats), "Dead")]
+		[HarmonyPostfix]
+		static void Dead(ref BabyStats __instance)
+		{
+			__instance.GetComponent<PhotonView>().RPC("DeadRPC", PhotonTargets.All);
+		}
+	}
+}
