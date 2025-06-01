7950fa8bf78567c401265b634342fba08dc0e290
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-05-31 20:04:45 -0400

    apparently i never even set this up right but it worked without it so i don't need it

--- HexaMod/Patches/Fixes/GhostFix.cs
+++ HexaMod/Patches/Fixes/GhostFix.cs
@@ -4,36 +4,9 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 namespace HexaMod.Patches.Fixes
 {
-	internal class PlayerRemover : MonoBehaviour // jane remover refernce 😱
-	{
-		void OnPhotonPlayerDisconnected(PhotonPlayer oldPlayer)
-		{
-			PhotonView netView = GetComponent<PhotonView>();
-
-			if (oldPlayer.ID == netView.ownerId)
-			{
-				FirstPersonController controller = GetComponent<FirstPersonController>();
-				controller.restrained = false;
-				controller.transform.parent = null;
-				controller.gameObject.SetActive(false);
-
-				Destroy(controller.gameObject);
-			}
-
-		}
-	}
-
 	[HarmonyPatch(typeof(FirstPersonController))]
 	internal class GhostFix
 	{
-
-		[HarmonyPatch("Start")]
-		[HarmonyPostfix]
-		static void Start()
-		{
-			
-		}
-
 		[HarmonyPatch("OnPhotonPlayerDisconnected")]
 		[HarmonyPrefix]
 		static bool OnPhotonPlayerDisconnected()
