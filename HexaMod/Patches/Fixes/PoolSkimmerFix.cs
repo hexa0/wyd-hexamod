7abda8ca9be0fe61427cbe6f132b108f39a11ca4
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-06-03 04:19:00 -0400

    fix pool skimmer glitching out

---
+++ HexaMod/Patches/Fixes/PoolSkimmerFix.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace HexaMod.Patches.Fixes
+{
+	[HarmonyPatch(typeof(LaserLight))]
+	internal class PoolSkimmerFix
+	{
+		[HarmonyPatch("Start")]
+		[HarmonyPostfix]
+		static void Update(ref LaserLight __instance)
+		{
+			if (__instance.isSkimmer)
+			{
+				Object.Destroy(__instance.transform.Find("PoolSkimmerHead").GetComponent<Rigidbody>());
+			}
+		}
+	}
+}
