cfeb633069cbbf88d8239c87a5ccfec4929f1321
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-06-03 04:08:40 -0400

    fix the clicking sound when the Game scene is loaded

---
+++ HexaMod/Patches/Fixes/StartClickFix.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace HexaMod.Patches.Fixes
+{
+	[HarmonyPatch(typeof(MicrowaveButton))]
+	internal class StartClickFix
+	{
+		[HarmonyPatch("Start")]
+		[HarmonyPostfix]
+		static void Start(ref MicrowaveButton __instance)
+		{
+			AudioSource clickSound = __instance.GetComponent<AudioSource>();
+			clickSound.playOnAwake = false;
+			clickSound.Stop();
+		}
+	}
+}
