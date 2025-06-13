3caf3cb48ed7956f4cc37da3df6aae06a1157db3
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-06-12 23:51:00 -0400

    chore sfx

--- HexaMod/Patches/Fixes/PutAwayChoreFix.cs
+++ HexaMod/Patches/Fixes/PutAwayChoreFix.cs
@@ -6,10 +6,27 @@ namespace HexaMod.Patches.Fixes
 	[HarmonyPatch(typeof(DishwareCount))]
 	internal class PutAwayChoreFix
 	{
+		[HarmonyPatch("AddDish")]
+		[HarmonyPrefix]
+		static void AddDish(object choreDoer, ref DishwareCount __instance)
+		{
+			if (__instance.curDishCount > 0)
+			{
+				DadPowerUps powerups = HexaMod.networkManager.playerObj.GetComponent<DadPowerUps>();
+				Traverse fields = Traverse.Create(powerups);
+				ActionText bigAction = fields.Field<GameObject>("bigAction").Value.GetComponent<ActionText>();
+				bigAction.ActionDone("Chore Completed");
+				var audio = bigAction.GetComponent<AudioSource>();
+				audio.clip = HexaMod.coreBundle.LoadAsset<AudioClip>($"Assets/ModResources/Core/Audio/Chore/ChoreCompletion{(__instance.curDishCount + 1) + (16 - __instance.totalDishes)}.wav");
+				audio.Play();
+			}
+		}
+
 		[HarmonyPatch("CheckIfDone")]
 		[HarmonyPrefix]
 		static bool CheckIfDone(object choreDoer, ref DishwareCount __instance)
 		{
+			// HexaMod.coreBundle.LoadAsset<AudioClip>("Assets/ModResources/Core/Audio/SmokeDetector.wav")
 			if (__instance.curDishCount >= __instance.totalDishes && !__instance.isDone)
 			{
 				Mod.Print($"Chore \"{__instance.smallActionMessage}\" has finished");
@@ -26,7 +43,9 @@ namespace HexaMod.Patches.Fixes
 					{
 						Mod.Print($"Trigger Random PowerUp!");
 						DadPowerUps powerups = HexaMod.networkManager.playerObj.GetComponent<DadPowerUps>();
-						powerups.DisplayComplete();
+						Traverse fields = Traverse.Create(powerups);
+						ActionText bigAction = fields.Field<GameObject>("bigAction").Value.GetComponent<ActionText>();
+						bigAction.ActionDone("Chore Completed");
 						powerups.SendMessage("RandomPowerUp");
 					}
 					else
