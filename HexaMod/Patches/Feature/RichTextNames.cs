92f1db2116c05d2407985505f809cd3777f56a11
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-06-13 18:43:25 -0400

    rich text name support

---
+++ HexaMod/Patches/Feature/RichTextNames.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using UnityEngine.UI;
+
+namespace HexaMod.Voice
+{
+	[HarmonyPatch]
+	internal class RichTextNames
+	{
+		[HarmonyPatch(typeof(PhotonNetworkManager), "Start")]
+		[HarmonyPrefix]
+		static void RefreshNameList(ref PhotonNetworkManager __instance)
+		{
+			__instance.lobbyInputField.characterLimit = 200;
+			__instance.lobbyInputField.textComponent.supportRichText = true;
+		}
+
+		[HarmonyPatch(typeof(PlayerNames), "RefreshNameList")]
+		[HarmonyPostfix]
+		static void RefreshNameList(ref PlayerNames __instance)
+		{
+			foreach (Text name in __instance.daddyNames)
+			{
+				name.supportRichText = true;
+			}
+
+			foreach (Text name in __instance.babyNames)
+			{
+				name.supportRichText = true;
+			}
+		}
+	}
+}
