bf00a13c9ada794e0e85bac8d4151e264fb481ed
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-06-16 02:25:40 -0400

    fix restrained jitter

---
+++ HexaMod/Patches/Fixes/FixRestrainedJitter.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace HexaMod.Patches.Fixes
+{
+	[HarmonyPatch(typeof(FirstPersonController))]
+	internal class FixRestrainedJitter
+	{
+		[HarmonyPatch("FixedUpdate")]
+		[HarmonyPostfix]
+		static void FixedUpdate(ref FirstPersonController __instance)
+		{
+			if (__instance.restrained || __instance.restrainedHeld)
+			{
+				__instance.transform.localPosition = Vector3.zero;
+				__instance.transform.localRotation = Quaternion.Euler(0f, 90f, 180f);
+
+				CharacterController controller = __instance.GetComponent<CharacterController>();
+
+				Traverse characterFields = Traverse.Create(__instance);
+				var m_MoveDir = characterFields.Field<Vector3>("m_MoveDir");
+
+				m_MoveDir.Value = Vector3.zero;
+
+				controller.center = controller.center;
+				controller.Move(Vector3.zero);
+			}
+		}
+	}
+}
