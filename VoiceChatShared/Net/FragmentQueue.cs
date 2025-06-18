96ba2d4e354a3a4ab4391e1f5fc30b582ce5e30c
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-06-18 19:09:41 -0400

    make VoiceChatShared one project & make it a submodule

--- VoiceChatShared/Net/FragmentQueue.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HexaVoiceChatShared.Net
-{
-	internal class FragmentQueue
-	{
-		public byte[] data = new byte[1024 * 4096];
-		public int dataOffest = 0;
-	}
-}
--- VoiceChatSharedNet9/Net/FragmentQueue.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HexaVoiceChatShared.Net
-{
-	internal class FragmentQueue
-	{
-		public byte[] data = new byte[1024 * 100];
-		public int dataOffest = 0;
-	}
-}
