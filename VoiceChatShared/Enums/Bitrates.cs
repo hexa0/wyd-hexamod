c9617549d9cb59c3482f3e3bd7d4df098bf5b931
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-07-13 09:01:10 -0400

    increase amount of bitrate options

--- VoiceChatShared/Enums/Bitrates.cs
+++ VoiceChatShared/Enums/Bitrates.cs
@@ -2,11 +2,17 @@
 {
 	public enum Bitrate : byte
 	{
+		Bitrate_Preset_6kbps = 6,
 		Bitrate_Preset_8kbps = 8,
 		Bitrate_Preset_16kbps = 16,
 		Bitrate_Preset_24kbps = 24,
 		Bitrate_Preset_48kbps = 48,
+		Bitrate_Preset_64kbps = 64,
+		Bitrate_Preset_96kbps = 96,
 		Bitrate_Preset_128kbps = 128,
-		Bitrate_Preset_240kbps = 240,
+		Bitrate_Preset_160kbps = 160,
+		Bitrate_Preset_192kbps = 192,
+		Bitrate_Preset_224kbps = 224,
+		Bitrate_Preset_255kbps = 255,
 	}
 }
