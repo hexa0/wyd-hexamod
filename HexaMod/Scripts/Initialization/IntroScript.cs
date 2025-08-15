710e772f169596c1222205430eea08dbf9d53f48
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-08-14 20:03:27 -0400

    fix lower replication rate, this did need to be set more often it seems

--- HexaMod/Scripts/Initialization/IntroScript.cs
+++ HexaMod/Scripts/Initialization/IntroScript.cs
@@ -24,6 +24,15 @@ namespace HexaMod.Scripts.Initialization
 			InitHexaMod();
 		}
 
+		void Update()
+		{
+			if (PhotonNetwork.sendRate != 30 || PhotonNetwork.sendRateOnSerialize != 30)
+			{
+				PhotonNetwork.sendRate = 30;
+				PhotonNetwork.sendRateOnSerialize = 30;
+			}
+		}
+
 		void SetLoadingText(string loadingText) => HexaMenus.startupScreen.loadingText.SetText(loadingText);
 
 		AsyncOperation sceneLoadOperation;
@@ -192,9 +201,6 @@ namespace HexaMod.Scripts.Initialization
 			QualitySettings.shadowResolution = ShadowResolution.Low;
 			QualitySettings.shadowDistance *= 0.75f;
 			QualitySettings.shadowProjection = ShadowProjection.CloseFit;
-
-			PhotonNetwork.sendRate = 30;
-			PhotonNetwork.sendRateOnSerialize = 30;
 
 			VoiceChat.Init();
 
