d10935bdd2d889467543947c52d188b0bfdf6dd9
hexa0 <67880600+hexa0@users.noreply.github.com>
2026-03-06 10:30:24 -0500

    remove this sad attempt at building with wine that i didn't even attempt lmao

--- HexaMod/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export WINEPREFIX="$HOME/.wine-dotnet-build"
-export WINEDEBUG=-all
-
-MSBUILD_PATH="C:/Windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe"
-
-wine "$MSBUILD_PATH" HexaMod.csproj /p:Configuration=Debug /p:Platform="Any CPU"
